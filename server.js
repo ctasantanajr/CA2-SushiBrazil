22aa08621885bbc766276b8999dafb1efb637244
ctasantanajr <ctasantanajr@gmail.com>
2020-04-23 19:13:40 +0000

    opening menu page

--- server.js
+++ server.js
@@ -3,16 +3,24 @@ cors = require("cors"),
 http = require("http"),
 express = require("express"),
 bodyParser = require("body-parser"),
-mongoose = require('mongoose');
+mongoose = require('mongoose'),
+path = require('path'), //The path module provides utilities for working with file and directory paths
+expAutoSan = require('express-autosanitizer'); //This module does the sanitisation of user input on the client-side
 require('dotenv').config();
 
 var app = express();
 var port = process.env.PORT || 3000;
-var itemCtrl = require('./item-controller');
+//var itemCtrl = require('./item-controller');
+
+app.use(express.static(path.resolve(__dirname, 'views'))); //We define the views folder as the one where all static content will be served
+app.use(express.urlencoded({extended: true})); //We allow the data sent from the client to be coming in as part of the URL in GET and POST requests
+app.use(express.json()); //We include support for JSON that is coming from the client
+app.use(expAutoSan.allUnsafe); //it sanitises all data coming from user input
 
 app.use(logger('dev'));
 app.use(bodyParser.json()); 
 app.use(require('./routes'));
+
 
 app.listen(port, function(err){
     console.log("Listening on Port: " + port)
@@ -25,4 +33,10 @@ mongoose.connection.on('error', (err) => {
 });
 mongoose.connection.on('connected', () => { 
     console.log('MongoDB is successfully connected');
+});
+
+app.get('/', function(req, res){
+
+    res.render('index');
+
 });
